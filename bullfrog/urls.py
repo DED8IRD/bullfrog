83b6b6d4f42071c040635ee1df4e7da6195cb523
DED8IRD <ewu72829@gmail.com>
2019-08-20 19:14:04 -0700

    fixed djangogirls path

--- bullfrog/urls.py
+++ bullfrog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('vue.urls')),
-    path('djangogirls', include('blog.urls')),
+    path('djangogirls/', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
